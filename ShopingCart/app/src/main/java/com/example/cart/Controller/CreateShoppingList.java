cd9d6aafab11a057590b46a2e68dfda0ea417103
Hasib-1997 <hasib1971.stu2016@juniv.edu>
2022-01-15 16:07:22 +0600

    Updated Create Shopping List

--- ShopingCart/app/src/main/java/com/example/cart/Controller/CreateShoppingList.java
+++ ShopingCart/app/src/main/java/com/example/cart/Controller/CreateShoppingList.java
@@ -3,10 +3,22 @@ package com.example.cart.Controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TableLayout;
 
 import com.example.cart.R;
+import com.google.firebase.database.DatabaseReference;
 
 public class CreateShoppingList extends AppCompatActivity {
+
+    TableLayout tableLayoutForCreateShoppingList;
+    DatabaseReference databaseReference;
+    Button submitDB;
+    Button addMoreItem;
+    
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
