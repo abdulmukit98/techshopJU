6a8beadb36ce56569a8e89ecfa6cc7667e6c1b01
Hasib-1997 <hasib1971.stu2016@juniv.edu>
2022-01-15 16:20:08 +0600

    CreateShoppingList Updated

--- ShopingCart/app/src/main/java/com/example/cart/Controller/CreateShoppingList.java
+++ ShopingCart/app/src/main/java/com/example/cart/Controller/CreateShoppingList.java
@@ -7,9 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 
+import com.example.cart.Model.common.ShoppingItem;
 import com.example.cart.R;
+import com.example.cart.ui.AddItemInShopTable;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateShoppingList extends AppCompatActivity {
 
@@ -34,8 +39,16 @@ public class CreateShoppingList extends AppCompatActivity {
         addMoreItem.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                AddItemInShopTable.addRow(view.getContext(), tableLayoutForCreateShoppingList, databaseReference);
+
             }
+        });
+
+        submitDB=findViewById(R.id.submit);
+        submitDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String, ShoppingItem> sales=new HashMap<>();
         });
 
 
