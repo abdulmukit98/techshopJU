f9e3470f13a8e136457a88b545796d28ba8eb985
Hasib-1997 <hasib1971.stu2016@juniv.edu>
2022-01-15 16:12:44 +0600

    Udated Create Shopping List again

--- ShopingCart/app/src/main/java/com/example/cart/Controller/CreateShoppingList.java
+++ ShopingCart/app/src/main/java/com/example/cart/Controller/CreateShoppingList.java
@@ -3,11 +3,13 @@ package com.example.cart.Controller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 
 import com.example.cart.R;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateShoppingList extends AppCompatActivity {
 
@@ -15,7 +17,7 @@ public class CreateShoppingList extends AppCompatActivity {
     DatabaseReference databaseReference;
     Button submitDB;
     Button addMoreItem;
-    
+
 
 
 
@@ -24,5 +26,21 @@ public class CreateShoppingList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_shopping_list);
+
+
+        tableLayoutForCreateShoppingList=findViewById(R.id.addOrderinTable);
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+        addMoreItem=findViewById(R.id.addMoreItem);
+        addMoreItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
+
+
+
     }
 }
