55262dd188ce9370bd0dc026b83f169096326155
Hasib-1997 <hasib1971.stu2016@juniv.edu>
2022-01-27 23:22:54 +0600

    New Class created and Updated

---
+++ ShopingCart/app/src/main/java/com/example/cart/CreateAndFillValuesInListShoppingRows.java
@@ -0,0 +1,79 @@
+package com.example.cart;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import com.example.cart.Model.ui.ExpandShopGroup;
+
+import java.util.ArrayList;
+
+public class CreateAndFillValuesInListShoppingRows extends BaseExpandableListAdapter {
+
+    private Context context;
+    private ArrayList<ExpandShopGroup> groups;
+
+    public CreateAndFillValuesInListShoppingRows(Context context, ArrayList<ExpandShopGroup> groups) {
+        this.context = context;
+        this.groups = groups;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return 0;
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return 0;
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return null;
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return null;
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isLastChaild, View view, ViewGroup parent) {
+        ExpandShopGroup group=(ExpandShopGroup) getGroup(groupPosition);
+        if (view==null)
+        {
+            LayoutInflater inf=(LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            
+        }
+
+        return null;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
