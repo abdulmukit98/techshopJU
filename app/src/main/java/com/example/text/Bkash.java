2950d8ee5322a31814e0556949af55c31478c703
Hasib-1997 <hasib1971.stu2016@juniv.edu>
2022-01-28 23:25:50 +0600

    updated

---
+++ app/src/main/java/com/example/text/Bkash.java
@@ -0,0 +1,19 @@
+package com.example.text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bkash extends AppCompatActivity {
+
+    /**
+     * Saving Instance for Bkash
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bkash);
+    }
+}
---
+++ app/src/main/java/com/example/text/Cash.java
@@ -0,0 +1,19 @@
+package com.example.text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cash extends AppCompatActivity {
+
+    /**
+     * Saving Instance for Cash
+     * @param savedInstanceState
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cash);
+    }
+}
